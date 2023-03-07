8033f241fa1adc635d86e7f91800292c65bbcb5c
Sarah Jacobs <sgjacobs14@gmail.com>
2023-03-07 18:48:39 -0500

    added mongo

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server for ${activity} running on port ${PORT}!`);
+  });
+});
